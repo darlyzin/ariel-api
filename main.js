3f857d81d850e76dc6f669cab798aab0d357752e
vinicius <vgalvaomarcelino@gmail.com>
2023-07-24 19:57:51 -0300

    c

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 import express from "express";
+import { Router } from "express";
 import fs from "fs";
 
 const app = express();
-const PORT = 3000;
 
-app.get("/kiss/images", authMiddleware, (req, res) => {
+const route = Router();
+const PORT = 4000;
+
+route.get("/kiss/images", (req, res) => {
   const { kissJSON } = JSON.parse(fs.readFileSync("./service/images.json", "utf8"));
 
   let randomGif = kissJSON[Math.floor(Math.random() * kissJSON.length)];
@@ -12,13 +15,15 @@ app.get("/kiss/images", authMiddleware, (req, res) => {
   return res.json(randomGif);
 });
 
-app.get("/hug/images", authMiddleware, (req, res) => {
+route.get("/hug/images", (req, res) => {
   const { hugJSON } = JSON.parse(fs.readFileSync("./service/images.json", "utf8"));
 
   let randomGif = hugJSON[Math.floor(Math.random() * hugJSON.length)];
 
   return res.json(randomGif);
 });
+
+app.use(route)
 
 app.listen(PORT, () => {
   console.log(`API de memes rodando em http://localhost:${PORT}`);
